d810a4aee9f8599160f50b02010fc977fa8b137a
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-11-07 22:15:43 +0530

    First Commit

---
+++ android/app/src/main/java/me/abhinavrajesh/news_app/MainActivity.java
@@ -0,0 +1,6 @@
+package me.abhinavrajesh.news_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/article_model.dart
@@ -0,0 +1,11 @@
+class ArticleModel {
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String content;
+
+  ArticleModel({ this.author, this.title, this.url, this.content, this.description, this.urlToImage});
+}
